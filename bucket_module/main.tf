4a1b2e63c0ce0fbe5bdfc16b3f0a04741b6913c7
K-taiga <taigakatou0329@gmail.com>
2020-12-30 19:30:17 +0900

    bucketをモジュール化

---
+++ bucket_module/main.tf
@@ -0,0 +1,44 @@
+variable "name" {}
+
+resource "aws_s3_bucket" "this" {
+  bucket = var.name
+
+  # バージョニングを有効にしオブジェクトの復元を可能とする
+  versioning {
+    enabled = true
+  }
+
+  # オブジェクト保存時に暗号化、参照時には復号する
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  # 180日後に自動で削除
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = "180"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket                  = aws_s3_bucket.this.id
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+
+output "arn" {
+  value = aws_s3_bucket.this.arn
+}
+
+output "name" {
+  value = aws_s3_bucket.this.id
+}
--- s3.tf
+++ s3.tf
@@ -1,37 +1,4 @@
-resource "aws_s3_bucket" "log" {
-  # 一意のバケット名
-  bucket = "kumaeers-log"
-
-  # バージョニングを有効にしオブジェクトの復元を可能とする
-  versioning {
-    enabled = true
-  }
-
-  # オブジェクト保存時に暗号化、参照時には復号する
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  # 180日後に自動で削除
-  lifecycle_rule {
-    enabled = true
-
-    expiration {
-      days = "180"
-    }
-  }
+module "log_bucket" {
+  source = "./buket_module"
+  name   = "kumaeers-log"
 }
-
-# S3へのアクセスが非公開でも保存しているオブジェクトが公開設定の場合にアクセスできない設定
-resource "aws_s3_bucket_public_access_block" "log" {
-  bucket                  = aws_s3_bucket.log.id
-  block_public_acls       = true
-  ignore_public_acls      = true
-  block_public_policy     = true
-  restrict_public_buckets = true
-}
-
